cc4d71669130f0d49a66449998d7725b292a207c
Didier Peran Ganthier <didierganthierperan@gmail.com>
2022-08-04 07:18:40 -0400

    tokens airdropped to contract

---
+++ scripts/9-setup-vote.js
@@ -0,0 +1,45 @@
+import sdk from "./1-initialize-sdk.js";
+
+// This is our governance contract.
+const vote = sdk.getVote("0x7CEd7D1e9788493d11662B63a111EC76cE7a6038");
+
+// This is our ERC-20 contract.
+const token = sdk.getToken("0x1A1d20A5E0e8a01fE20A57Aca65CC3564Eb1B8F7");
+
+(async () => {
+  try {
+    // Give our treasury the power to mint additional token if needed.
+    await token.roles.grant("minter", vote.getAddress());
+
+    console.log(
+      "Successfully gave vote contract permissions to act on token contract"
+    );
+  } catch (error) {
+    console.error(
+      "failed to grant vote contract permissions on token contract",
+      error
+    );
+    process.exit(1);
+  }
+
+  try {
+    // Grab our wallet's token balance, remember -- we hold basically the entire supply right now!
+    const ownedTokenBalance = await token.balanceOf(
+      process.env.WALLET_ADDRESS
+    );
+
+    // Grab 90% of the supply that we hold.
+    const ownedAmount = ownedTokenBalance.displayValue;
+    const percent90 = Number(ownedAmount) / 100 * 90;
+
+    // Transfer 90% of the supply to our voting contract.
+    await token.transfer(
+      vote.getAddress(),
+      percent90
+    ); 
+
+    console.log("✅ Successfully transferred " + percent90 + " tokens to vote contract");
+  } catch (err) {
+    console.error("failed to transfer tokens to vote contract", err);
+  }
+})();
